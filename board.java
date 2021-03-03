15523ad52368651814b9042b72e74c6878f2de41
cl1217 <cl1217@scarletmail.rutgers.edu>
2021-03-02 20:51:47 -0500

    created intial classes for chess game

---
+++ board.java
@@ -0,0 +1,9 @@
+public class board
+{
+	//private fields go here
+	
+	//constructor goes here
+	public board()
+	
+	
+}
---
+++ chess.java
@@ -0,0 +1,18 @@
+import java.io;
+import java.util.*;
+
+public class chess
+{
+	public static void main(String[]args)
+	{
+		
+		
+		String userInput;
+		Scanner scan = System.in(userInput);
+		
+		
+		
+		
+	}
+	
+}
