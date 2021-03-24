02bce66986c96e335559ca354046c8a5a3c718e6
cl1217 <cl1217@scarletmail.rutgers.edu>
2021-03-24 13:12:44 -0400

    added javadoc for piece class

--- piece.java
+++ piece.java
@@ -1,3 +1,14 @@
+/**
+* Piece object that represents a chess piece.
+*
+* A <code>piece</code> object has qualities of a chess piece, which includes its
+* color, its piece type, and its location on the chess board.
+*
+* @author Charles Li
+* @author Max Sun
+* @version 1.2 Mar 2021
+*/
+
 public class piece
 {
 	private char color;
@@ -6,6 +17,15 @@ public class piece
 	private int l;
 	private boolean castle;
 	
+	/**
+	* 
+	* Default <code>piece</code> constructor.
+	*
+	* @param color color of the chess piece
+	* @param name the type of chess piece, abbreviated as a single character
+	* @param n the vertical coordinate of the piece on the board
+	* @param l the horizontal coordinate of the piece on the board
+	*/
 	public piece(char color, char name, int n, int l)
 	{
 		this.color = color;
@@ -18,45 +38,95 @@ public class piece
 		}
 	}
 	
-	
-	
-	
+	/**
+	* Gets the color of the chess piece.
+	*
+	* @return the color of the chess piece
+	*/
 	public char getColor()
 	{
 		return color;
 	}
 	
+	/**
+	* Gets the name of the chess piece.
+	*
+	* @return the name of the chess piece
+	*/
 	public char getName()
 	{
 		return name;
 	}
 	
+	/**
+	* Gets the vertical coordinate of the chess piece.
+	*
+	* @return the vertical coordinate of the chess piece
+	*/
 	public int getN()
 	{
 		return n;
 	}
+	
+	/**
+	* Gets the horizontal coordinate of the chess piece.
+	*
+	* @return the horizontal coordinate of the chess piece
+	*/
 	public int getL()
 	{
 		return l;
 	}
 	
+	/**
+	* Sets the vertical coordinate of the chess piece.
+	*
+	* @param n the desired vertical coordinate of the chess piece
+	*/
 	public void setN(int n)
 	{
 		this.n = n;
 	}
 	
+	/**
+	* Sets the horizontal coordinate of the chess piece.
+	*
+	* @param l the desired horizontal coordinate of the chess piece
+	*
+	*/
 	public void setL(int l)
 	{
 		this.l = l;
 	}
-	public void setCastle(boolean castle)
-	{
-		this.castle = castle;
-	}
+	
+	/**
+	* Gets the castle capability of the chess piece. This is to be used with
+	* Kings and Rooks.
+	*
+	* @return the castle capability of the chess piece
+	*/
 	public boolean canCastle()
 	{
 		return castle;
 	}
+	
+	/**
+	* Sets the castle capability of the chess piece. This is to be used with
+	* Kings and Rooks.
+	*
+	* @param castle the desired castle capability of the chess piece
+	*
+	*/
+	public void setCastle(boolean castle)
+	{
+		this.castle = castle;
+	}
+	
+	/**
+	* Gets the chess piece represented as a String. 
+	*
+	* @return the chess piece as a <code>String</code>
+	*/
 	public String toString()
 	{
 		return "" + color + name;
